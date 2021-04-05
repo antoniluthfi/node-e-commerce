916c7074224c2e91f9c20c2450ad8cd497f2b9e0
antoniluthfi <luthfiantoni027@gmail.com>
2021-04-05 08:04:37 +0700

    setting auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {createOrUpdateUser} = require('../controllers/auth');
+// middleware
+const { authCheck } = require('../middlewares/auth');
 
-router.get('/login', createOrUpdateUser);
+// controllers
+const { createOrUpdateUser } = require('../controllers/auth');
+
+router.get('/create-or-update-user', authCheck, createOrUpdateUser);
 
 module.exports = router;
